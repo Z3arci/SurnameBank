ba76fca0440daef37ab5852f2c5267202cd33aac
neopihanov.av <kasov94@inbox.ru>
2024-03-20 13:08:18 +0300

    Card

--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -1,4 +1,34 @@
 package data;
 
+import bank.Card;
+
 public class Client {
+    private String name;
+    private String surname;
+    private boolean isClient;
+    private long Card;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public boolean isClient() {
+        return isClient;
+    }
+
+    public void setClient(boolean client) {
+        isClient = client;
+    }
+
+    public long getCard() {
+        return Card;
+    }
+
+    public void setCard(long card) {
+        Card = card;
+    }
 }
